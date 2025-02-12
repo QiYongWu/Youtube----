6e15b40c8f1fe3bb1cbe6ed73bc41686e931d69a
QiYongWu <qiyongw@126.com>
2025-02-12 14:32:01 +0800

    Add informations about all videos

---
+++ data/imformations.js
@@ -0,0 +1,123 @@
+const imformations = [
+  {
+     authorName : "Marques Brownlee",
+     title : "Talking Tech and AI with Google CEO Sundar Pichai!",
+     views : "3.4M views · 6 months ago",
+     videoLink : "https://www.youtube.com/watch?v=n2RNcPRtAiY",
+     videoImage : "../All images/images/image1.png",
+     videoTimes : "14:20",
+     authorImage : "../All images/images/author1.png"
+   },
+   {
+    authorName : "Markiplier",
+    title : "Try Not To Laugh Challenge #9",
+    views : "19M views · 4 years ago",
+    videoLink : "https://www.youtube.com/watch?v=mP0RAo9SKZk",
+    videoTimes : "8:22",
+    videoImage : "../All images/images/author2.png",
+    authorImage : "../All images/images/author2.png"
+  },
+
+  {
+    authorName : "SSSniperWolf",
+    title : "Crazy Tik Toks Taken Moments Before DISASTER",
+    views : "12M views · 1 year ago",
+    videoLink : "https://www.youtube.com/watch?v=FgjPQQeTh1w",
+    videoTimes : "9:13",
+    videoImage : "../All images/images/author3.png",
+    authorImage : "../All images/images/author3.png"
+  },
+
+  {
+    authorName : "Veritasium",
+    title : "The Simplest Math Problem No One Can Solve - Collatz Conjecture",
+    views : "18M views · 4 months ago",
+    videoLink : "https://www.youtube.com/watch?v=094y1Z2wpJg",
+    videoTimes : "22:09",
+    videoImage : "../All images/images/author4.png",
+    authorImage : "../All images/images/author4.png"
+  },
+
+
+  {
+    authorName : "CS Dojo",
+    title : "Kadane's Algorithm to Maximum Sum Subarray Problem",
+    views : "519K views · 5 years ago",
+    videoLink : "https://www.youtube.com/watch?v=86CQq3pKSUw",
+    videoTimes : "11:17",
+    videoImage : "../All images/images/author5.png",
+    authorImage : "../All images/images/author5.png"
+  },
+
+  {
+    authorName : "MrBeast",
+    title : "Anything You Can Fit In The Circle I’ll Pay For",
+    views : "141M views · 1 year ago",
+    videoLink : "https://www.youtube.com/watch?v=yXWw0_UfSFg",
+    videoTimes : "11:17",
+    videoImage : "../All images/images/author6.png",
+    authorImage : "../All images/images/author6.png"
+  },
+
+  {
+    authorName : "RealLifeLore",
+    title : "Why Planes Don't Fly Over Tibet",
+    views : "6.6M views · 1 year ago",
+    videoLink : "https://www.youtube.com/watch?v=fNVa1qMbF9Y",
+    videoTimes : "10:13",
+    videoImage : "../All images/images/author7.png",
+    authorImage : "../All images/images/author7.png"
+  },
+
+  {
+    authorName : "Tech Vision",
+    title : "Inside The World's Biggest Passenger Plane",
+    views : "3.7M views · 10 months ago",
+    videoLink : "https://www.youtube.com/watch?v=lFm4EM1juls",
+    videoTimes : "7:12",
+    videoImage : "../All images/images/author8.png",
+    authorImage : "../All images/images/author8.png"
+  },
+
+  {
+    authorName : "ThenX",
+    title : "The SECRET to Super Human STRENGTH",
+    views : "20M views · 3 year ago",
+    videoLink : "https://www.youtube.com/watch?v=ixmxOlcrlUc",
+    videoTimes : "13:17",
+    videoImage : "../All images/images/author9.png",
+    authorImage : "../All images/images/author9.png"
+  },
+
+  {
+    authorName : "Business Insider",
+    title : "How The World's Largest Cruise Ship Makes 30,000 Meals Every Day",
+    views : "14M views · 1 year ago",
+    videoLink : "https://www.youtube.com/watch?v=R2vXbFp5C9o",
+    videoTimes : "7:53",
+    videoImage : "../All images/images/author10.png",
+    authorImage : "../All images/images/author10.png"
+  },
+
+  {
+    authorName : "Destination Tips",
+    title : "Dubai's Crazy Underwater Train and Other Things #Only in Dubai",
+    views : "3M views · 1 year ago",
+    videoLink : "https://www.youtube.com/watch?v=0nZuYyXET3s",
+    videoTimes : "4:10",
+    videoImage : "../All images/images/author11.png",
+    authorImage : "../All images/images/author11.png"
+  },
+
+  {
+    authorName : "TED-Ed",
+    title : "What would happen if you didn’t drink water? - Mia Nacamulli",
+    views : "12M views · 5 years ago",
+    videoLink : "https://www.youtube.com/watch?v=9iMGFqMmUFs",
+    videoTimes : "4:51",
+    videoImage : "../All images/images/author12.png",
+    authorImage : "../All images/images/author12.png"
+  },
+
+
+];
